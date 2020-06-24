cf8de17f155df0eb4b042e855613d3a7cbcb1db8
dspec12 <danspec12@gmail.com>
2020-06-23 21:48:26 -0400

    fixed vpc_id reference

--- aws/eks_clusters/utils_cluster.tf
+++ aws/eks_clusters/utils_cluster.tf
@@ -97,7 +97,7 @@ resource "aws_security_group" "worker_group_mgmt_two" {
 
 resource "aws_security_group" "all_worker_mgmt" {
   name_prefix = "all_worker_management"
-  vpc_id      = module.vpc.vpc_id
+  vpc_id      = module.eks.vpc_id
 
   ingress {
     from_port = 22
