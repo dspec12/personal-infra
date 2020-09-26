414517615177597e2c04a6ce745aef7c4cdff434
dspec12 <danspec12@gmail.com>
2020-09-26 11:10:06 -0400

    updated kubevpc

--- aws/networking/main.tf
+++ aws/networking/main.tf
@@ -25,44 +25,30 @@ data "aws_availability_zones" "available" {}
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "2.43.0"
+  version = "2.54.0"
 
-  name = "shared-services-${var.env_stage}"
+  name = "KubeVPC"
   cidr = "10.0.0.0/16"
 
   azs = data.aws_availability_zones.available.names
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-  database_subnets = ["10.0.10.0/24", "10.0.11.0/24", "10.0.13.0/24"]
+  public_subnets  = ["10.0.101.0/24"]
 
 private_subnet_tags =  {
   Envirnment = "${var.env_stage}"
   Type = "Private"
   "kubernetes.io/role/internal-elb" = "1"
-  # "kubernetes.io/cluster/eksctl-test-cluster-cluster" = "shared"
-  # "kubernetes.io/cluster/test-cluster" = "shared"
 }
 
 public_subnet_tags =  {
   Envirnment = "${var.env_stage}"
   Type = "Public"
   "kubernetes.io/role/elb" = "1"
-  # "kubernetes.io/cluster/test-cluster" = "shared"
-}
-
-database_subnet_tags =  {
-  Envirnment = "${var.env_stage}"
-  Type = "Database"
 }
 
   enable_nat_gateway = true
-  enable_vpn_gateway = true
-  create_database_subnet_group = true
+  single_nat_gateway = true
   enable_dns_hostnames = true
   enable_dns_support   = true
 
-  # VPC endpoint for KMS
-  enable_kms_endpoint = true
-  kms_endpoint_private_dns_enabled = true
-  kms_endpoint_security_group_ids = [data.aws_security_group.default.id]
 }
